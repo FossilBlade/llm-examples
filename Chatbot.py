c177f11a05ced4239713553d2197dda65f869617
FossilBlade <32032862+FossilBlade@users.noreply.github.com>
2025-02-12 09:22:27 +0000

    model change

--- Chatbot.py
+++ Chatbot.py
@@ -44,7 +44,7 @@ prompt = ChatPromptTemplate.from_messages(template)
 
 
 model = ChatOpenAI(
-    model="gpt-4o",
+    model="gpt-4o-mini",
     temperature=0,
     max_tokens=None,
     timeout=None,
